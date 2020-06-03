2f54941e9955f3ab9e0f9115172450176c1ec882
Kévin FISCHER <kfischer@smag.tech>
2020-06-03 13:11:49 +0200

    add sass.bat + sass.sh + package.json + main.scss

---
+++ sass.bat
@@ -0,0 +1,44 @@
+mkdir abstracts
+mkdir base
+mkdir components
+mkdir layout
+mkdir pages
+mkdir themes
+mkdir vendors
+cd abstracts
+echo. > _variables.scss
+echo. > _function.scss
+echo. > _mixins.scss
+echo. > _placeholders.scss
+cd .. 
+cd base 
+echo. > _animations.scss
+echo. > _typography.scss
+echo. > _utilities.scss
+echo. > _reset.scss
+cd ..
+cd components
+echo. > _buttons.scss
+echo. > _carousel.scss
+echo. > _cover.scss
+echo. > _dropdown.scss
+cd ..
+cd layout
+echo. > _navigation.scss
+echo. > _grid.scss
+echo. > _header.scss
+echo. > _footer.scss
+echo. > _sidebar.scss
+echo. > _forms.scss
+cd ..
+cd pages
+echo. > _home.scss
+echo. > _contact.scss
+cd ..
+cd themes
+echo. > _theme.scss
+echo. > _admin.scss
+cd ..
+cd vendors
+echo. > _bootstrap.scss
+echo. > _jquery.scss
